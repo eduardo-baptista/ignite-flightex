8c1ffeaf9abfb404d60eb02e890e1a73d613ca2d
Eduardo Baptista <eduardo.baptista@vulcanet.com.br>
2021-05-16 15:18:45 -0300

    Implements booking struct

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,23 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  @type t :: %__MODULE__{
+          complete_date: NaiveDateTime.t(),
+          local_origin: String.t(),
+          local_destination: String.t(),
+          user_id: binary(),
+          id: binary()
+        }
+
+  @spec build(NaiveDateTime.t(), String.t(), String.t(), binary()) :: {:ok, __MODULE__.t()}
+  def build(complete_date, local_origin, local_destination, user_id) do
+    {:ok,
+     %__MODULE__{
+       complete_date: complete_date,
+       local_origin: local_origin,
+       local_destination: local_destination,
+       user_id: user_id,
+       id: UUID.uuid4()
+     }}
   end
 end
